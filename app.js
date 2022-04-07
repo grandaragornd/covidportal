89c25308e5e9a083ca11ef1007067e01c461f8ac
unknown <kevinpereaenriquez@gmail.com>
2022-04-07 09:48:18 +0200

    covidportal netify

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app = express()
 const port = 3000;
+const mongoose = require('mongoose')
+
+mongoose.connect('mongodb+srv://saintkevinst:5122012@cluster0.mbhd5.mongodb.net/covidportal')
 
 app.set('view engine', 'ejs');
 app.set('views',__dirname + '/views')
